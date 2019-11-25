e217e0644e14174ec4b6879ac946b3526489f2f4
mdilauro39 <mdilauro39@gmail.com>
2019-11-25 20:28:14 -0300

    hasta aca la mitad de la parte 4, falta poner redirigir a la pagina siguiente

--- pps/__init__.py
+++ pps/__init__.py
@@ -7,14 +7,21 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 #importamos migracion
 from flask_migrate import Migrate
+#imnportamos Loginmanager
+from flask_login import LoginManager
 #creamos una instancia de Flask
 app = Flask(__name__)
 #importamos de config.py el objeto Config
 app.config.from_object(Config)
-#definimos 
+#definimos Sqlalchemy 
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+#definimos instancia de Loginmanager
+login = LoginManager(app)
 
 from pps import routes, models
 #importamos del sub-directorio pps routes.py
+
+login = LoginManager(app)
+login.login_view = 'login'
--- pps/routes.py
+++ pps/routes.py
@@ -4,12 +4,20 @@ from flask import render_template, flash, redirect, url_for
 #importamos de pps app
 from pps import app
 #importamos de forms.py la funcion LoginForm
+#lógica de función de vista de inicio de sesión
 from pps.forms import LoginForm
+from flask_login import current_user, login_user
+from pps.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
 #usamos un decorator para la ruta /
 @app.route('/')
 #usamos un decorator para la ruta /index
 @app.route('/index')
 #definimos una funcion que se ejecuta al solicitar esta ruta
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -28,9 +36,19 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
